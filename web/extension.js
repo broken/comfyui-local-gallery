38998c47d888d491e13148d38ca8f19dc8997f60
dogatech <dogatech@dogatech.home>
2026-04-26 19:58:13 -0700

    switch icon

--- web/extension.js
+++ web/extension.js
@@ -4,9 +4,10 @@ const BUTTON_TOOLTIP = "Launch Local Gallery";
 
 const getGalleryIcon = () => {
     return `
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="3" y="3" width="18" height="18" rx="4" stroke="white" stroke-width="2"/>
-            <path d="M15 9H11C9.89543 9 9 9.89543 9 11V13C9 14.1046 9.89543 15 11 15H15V13H12" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+            <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+            <circle cx="8.5" cy="8.5" r="1.5"></circle>
+            <polyline points="21 15 16 10 5 21"></polyline>
         </svg>
     `;
 };
